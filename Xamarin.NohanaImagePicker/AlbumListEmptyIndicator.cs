f9ffcddc4be490a976e9c3ffcaa266474471d09d
AJ Rotolo <aj.rotolo@eightbot.com>
2017-09-15 01:40:44 -0500

    corrected attributed string setup in AlbumListEmptyIndicator

--- Xamarin.NohanaImagePicker/AlbumListEmptyIndicator.cs
+++ Xamarin.NohanaImagePicker/AlbumListEmptyIndicator.cs
@@ -14,22 +14,28 @@ namespace Xamarin.NohanaImagePicker
             var centerStyle = new NSMutableParagraphStyle();
             centerStyle.Alignment = UITextAlignment.Center;
 
-            var messageAttributes = new NSDictionary<NSString, NSObject>();
-            messageAttributes.SetValueForKey(config.Color.Empty ?? new UIColor(red: 0x88 / 0xff, green: 0x88 / 0xff, blue: 0x88 / 0xff, alpha: 1), UIStringAttributeKey.ForegroundColor);
-            messageAttributes.SetValueForKey(UIFont.SystemFontOfSize(14), UIStringAttributeKey.Font);
-            messageAttributes.SetValueForKey(centerStyle, UIStringAttributeKey.ParagraphStyle);
+            var messageAttributes = new UIStringAttributes
+            {
+                ForegroundColor = config.Color.Empty ?? new UIColor(red: 0x88 / 0xff, green: 0x88 / 0xff, blue: 0x88 / 0xff, alpha: 1),
+                Font = UIFont.SystemFontOfSize(26),
+                ParagraphStyle = centerStyle
+            };
 
             var messageText = new NSAttributedString(message, messageAttributes);
 
-            var descriptionAttributes = new NSDictionary<NSString, NSObject>();
-            descriptionAttributes.SetValueForKey(config.Color.Empty ?? new UIColor(red: 0x88 / 0xff, green: 0x88 / 0xff, blue: 0x88 / 0xff, alpha: 1), UIStringAttributeKey.ForegroundColor);
-			descriptionAttributes.SetValueForKey(UIFont.SystemFontOfSize(14), UIStringAttributeKey.Font);
-			descriptionAttributes.SetValueForKey(centerStyle, UIStringAttributeKey.ParagraphStyle);
+            var descriptionAttributes = new UIStringAttributes
+            {
+                ForegroundColor = config.Color.Empty ?? new UIColor(red: 0x88 / 0xff, green: 0x88 / 0xff, blue: 0x88 / 0xff, alpha: 1),
+                Font = UIFont.SystemFontOfSize(14),
+                ParagraphStyle = centerStyle
+            };
 
             var descriptionText = new NSAttributedString(description, descriptionAttributes);
 
-            var attributedTextAttributes = new NSDictionary<NSString, NSObject>();
-            attributedTextAttributes.SetValueForKey(UIFont.SystemFontOfSize(14), UIStringAttributeKey.Font);
+            var attributedTextAttributes = new UIStringAttributes
+            {
+                Font = UIFont.SystemFontOfSize(14)
+            };
 
             var attributedText = new NSMutableAttributedString();
             attributedText.Append(messageText);
